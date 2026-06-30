222443c2e06a94c74dc9af2dfe4cf1ed75c98088
ironikhil <nikhilsharmaxd@gmail.com>
2026-06-30 11:39:44 +0530

    Time: 2 ms (99.94%), Space: 52.6 MB (40.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,22 +1,46 @@
+import java.util.*;
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list= new ArrayList<>();
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for( int i=0;i<nums.length; i++){
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i],hm.get(nums[i])+1);
+        int c1=0; int c2=0;
+        int e1=Integer.MAX_VALUE;
+        int e2=Integer.MAX_VALUE;
+        for ( int i=0; i< nums.length; i++){
+            if(c1==0 && nums[i]!=e2){
+                c1=1;
+                e1=nums[i];
             }
-            else{
-                hm.put(nums[i],1);
+            else if(c2==0 && nums[i]!=e1){
+                c2=1;
+                e2=nums[i];
+            }
+            else if(e1==nums[i]){
+                c1++;
+            }
+            else if(e2==nums[i]){
+                c2++;
+            }
+            else {
+                c1--;c2--;
             }
         }
-        int n=nums.length;
+        int co1=0;
+        int co2=0;
+        List<Integer> list = new ArrayList<>();
         for( int x : nums){
-            if(hm.get(x)>n/3){
-                if(!list.contains(x))
-                    list.add(x);
+            if(x==e1){
+                co1++;
             }
+            if(x==e2){
+                co2++;
+            }
+            
         }
+        if(co1>nums.length/3){
+                list.add(e1);
+            }
+            if(co2>nums.length/3){
+                list.add(e2);
+            }
         return list;
 
     }
